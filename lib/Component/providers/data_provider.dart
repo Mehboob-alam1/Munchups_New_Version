8d283876098359d75ef5880b96b881d4da8dc2ae
Mehboob-alam1 <ma8122576@gmail.com>
2025-09-04 20:44:53 +0500

    some more fixes

--- lib/Component/providers/data_provider.dart
+++ lib/Component/providers/data_provider.dart
@@ -1,247 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:dartz/dartz.dart';
-import 'dart:convert';
-import 'package:shared_preferences/shared_preferences.dart';
-import '../../core/error/failures.dart';
-import '../../domain/usecases/data/fetch_home_data_usecase.dart';
-import '../../domain/usecases/data/fetch_user_profile_usecase.dart';
-import '../../domain/usecases/data/search_users_usecase.dart';
-import '../../domain/usecases/data/fetch_notifications_usecase.dart';
+import 'package:provider/provider.dart';
+import '../../presentation/providers/data_provider.dart';
+import 'app_provider.dart';
+import 'auth_provider.dart';
+import 'cart_provider.dart';
+import 'data_provider.dart';
 
-class DataProvider extends ChangeNotifier {
-  bool _isLoading = false;
-  String _error = '';
+class MainProvider extends StatelessWidget {
+  final Widget child;
 
-  // Home data
-  List<dynamic> _homeData = [];
-  List<dynamic> _chefsList = [];
-  List<dynamic> _grocersList = [];
-
-  // Search data
-  List<dynamic> _searchResults = [];
-  String _searchQuery = '';
-
-  // Profile data
-  Map<String, dynamic> _userProfile = {};
-
-  // Notifications
-  List<dynamic> _notifications = [];
-  int _unreadCount = 0;
-
-  // Use Cases
-  final FetchHomeDataUseCase fetchHomeDataUseCase;
-  final FetchUserProfileUseCase fetchUserProfileUseCase;
-  final SearchUsersUseCase searchUsersUseCase;
-  final FetchNotificationsUseCase fetchNotificationsUseCase;
-
-  DataProvider({
-    required this.fetchHomeDataUseCase,
-    required this.fetchUserProfileUseCase,
-    required this.searchUsersUseCase,
-    required this.fetchNotificationsUseCase,
+  const MainProvider({
+    super.key,
+    required this.child,
   });
 
-  // Getters
-  bool get isLoading => _isLoading;
-  String get error => _error;
-  List<dynamic> get homeData => _homeData;
-  List<dynamic> get chefsList => _chefsList;
-  List<dynamic> get grocersList => _grocersList;
-  List<dynamic> get searchResults => _searchResults;
-  String get searchQuery => _searchQuery;
-  Map<String, dynamic> get userProfile => _userProfile;
-  List<dynamic> get notifications => _notifications;
-  int get unreadCount => _unreadCount;
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => DataProvider()),
 
-  // Loading state
-  void _setLoading(bool loading) {
-    _isLoading = loading;
-    notifyListeners();
+        ChangeNotifierProvider(create: (_) => AppProvider()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+      ],
+      child: child,
+    );
   }
+}
 
-  // Error handling
-  void _setError(String error) {
-    _error = error;
-    notifyListeners();
-  }
+// Extension methods for easy access to providers
+extension ProviderExtension on BuildContext {
+  AppProvider get appProvider => Provider.of<AppProvider>(this, listen: false);
+  AuthProvider get authProvider => Provider.of<AuthProvider>(this, listen: false);
+  CartProvider get cartProvider => Provider.of<CartProvider>(this, listen: false);
 
-  // Clear error
-  void clearError() {
-    _error = '';
-    notifyListeners();
-  }
+  DataProvider get dataProvider => Provider.of<DataProvider>(this, listen: false);
 
-  // Home data methods
-  Future<void> fetchHomeData() async {
-    _setLoading(true);
-    _setError('');
-
-    try {
-      // Get user data and location from SharedPreferences
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      String? userDataString = prefs.getString('data');
-      String? latlongString = prefs.getString('guestLatLong');
-
-      Map<String, dynamic>? params;
-
-      if (userDataString != null) {
-        // User is logged in
-        Map<String, dynamic> userData = jsonDecode(userDataString);
-        params = {
-          'userId': userData['user_id'].toString(),
-        };
-      } else if (latlongString != null) {
-        // Guest user with location
-        Map<String, dynamic> latlong = jsonDecode(latlongString);
-        params = {
-          'location': {
-            'lat': latlong['lat'].toString(),
-            'long': latlong['long'].toString(),
-          },
-        };
-      } else {
-        throw Exception('No user data or location available');
-      }
-
-      final result = await fetchHomeDataUseCase(params);
-
-      result.fold(
-            (failure) {
-          _setError(failure.message);
-        },
-            (success) {
-          _homeData = success['data'] ?? [];
-          _chefsList = success['chefs'] ?? [];
-          _grocersList = success['grocers'] ?? [];
-        },
-      );
-    } catch (e) {
-      _setError(e.toString());
-      debugPrint('Error fetching home data: $e');
-    } finally {
-      _setLoading(false);
-    }
-  }
-
-  // Profile methods
-  Future<void> fetchUserProfile() async {
-    _setLoading(true);
-    _setError('');
-
-    try {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      String? userDataString = prefs.getString('data');
-
-      if (userDataString != null) {
-        Map<String, dynamic> userData = jsonDecode(userDataString);
-        String userId = userData['user_id'].toString();
-        String userType = userData['user_type'].toString();
-
-        final result = await fetchUserProfileUseCase({
-          'userId': userId,
-          'userType': userType,
-        });
-
-        result.fold(
-              (failure) {
-            _setError(failure.message);
-          },
-              (success) {
-            _userProfile = success;
-          },
-        );
-      } else {
-        throw Exception('No user data available');
-      }
-    } catch (e) {
-      _setError(e.toString());
-      debugPrint('Error fetching user profile: $e');
-    } finally {
-      _setLoading(false);
-    }
-  }
-
-  // Search methods
-  Future<void> searchUsers(String query) async {
-    _setLoading(true);
-    _setError('');
-    _searchQuery = query;
-
-    try {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      String? userDataString = prefs.getString('data');
-      String? userId = userDataString != null
-          ? jsonDecode(userDataString)['user_id'].toString()
-          : null;
-
-      final result = await searchUsersUseCase({
-        'query': query,
-        'userId': userId,
-      });
-
-      result.fold(
-            (failure) {
-          _setError(failure.message);
-        },
-            (success) {
-          _searchResults = success;
-        },
-      );
-    } catch (e) {
-      _setError(e.toString());
-      debugPrint('Error searching users: $e');
-    } finally {
-      _setLoading(false);
-    }
-  }
-
-  // Notification methods
-  Future<void> fetchNotifications() async {
-    _setLoading(true);
-    _setError('');
-
-    try {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      String? userDataString = prefs.getString('data');
-
-      if (userDataString != null) {
-        Map<String, dynamic> userData = jsonDecode(userDataString);
-        String userId = userData['user_id'].toString();
-
-        final result = await fetchNotificationsUseCase(userId);
-
-        result.fold(
-              (failure) {
-            _setError(failure.message);
-          },
-              (success) {
-            _notifications = success;
-            _unreadCount = _notifications.where((n) => n['is_read'] != '1').length;
-          },
-        );
-      } else {
-        throw Exception('No user data available');
-      }
-    } catch (e) {
-      _setError(e.toString());
-      debugPrint('Error fetching notifications: $e');
-    } finally {
-      _setLoading(false);
-    }
-  }
-
-  // Initialize all data
-  Future<void> initializeData() async {
-    await Future.wait([
-      fetchHomeData(),
-      fetchNotifications(),
-    ]);
-  }
-
-  // Clear search
-  void clearSearch() {
-    _searchQuery = '';
-    _searchResults = [];
-    notifyListeners();
-  }
+  // Listenable versions for UI updates
+  AppProvider get appProviderListen => Provider.of<AppProvider>(this);
+  AuthProvider get authProviderListen => Provider.of<AuthProvider>(this);
+  CartProvider get cartProviderListen => Provider.of<CartProvider>(this);
+  DataProvider get dataProviderListen => Provider.of<DataProvider>(this);
 }
--- lib/Component/providers/main_provider.dart
+++ lib/Component/providers/main_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../../presentation/providers/data_provider.dart';
 import 'app_provider.dart';
 import 'auth_provider.dart';
 import 'cart_provider.dart';
