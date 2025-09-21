29846d07b4c53d7df713a44ad5b8232c29db1955
Mehboob-alam1 <ma8122576@gmail.com>
2025-09-21 16:05:07 +0500

    login resp

--- lib/data/models/auth_response_model.dart
+++ lib/data/models/auth_response_model.dart
@@ -24,19 +24,32 @@ class AuthResponseModel {
   factory AuthResponseModel.fromJson(Map<String, dynamic> json) {
     // Handle myFollowers conversion safely to prevent "string is not subtype of int" error
     int? myFollowers;
-    if (json['myFollowers'] != null) {
-      if (json['myFollowers'] is String) {
-        myFollowers = int.tryParse(json['myFollowers']);
-      } else if (json['myFollowers'] is num) {
-        myFollowers = json['myFollowers'].toInt();
+    if (json['my_followers'] != null) {
+      if (json['my_followers'] is String) {
+        myFollowers = int.tryParse(json['my_followers']);
+      } else if (json['my_followers'] is num) {
+        myFollowers = json['my_followers'].toInt();
       }
     }
 
+    // Create user data from the response fields
+    Map<String, dynamic> userData = {
+      'user_id': json['user_id'],
+      'first_name': json['first_name'],
+      'last_name': json['last_name'],
+      'user_name': json['user_name'],
+      'email': json['email'],
+      'phone': json['phone'],
+      'image': json['image'],
+      'connect_with_stripe': json['connect_with_stripe'],
+      'signup_status': json['signup_status'],
+    };
+
     return AuthResponseModel(
-      status: json['status']?.toString() ?? '',
-      message: json['message']?.toString() ?? '',
-      data: json['data'] as Map<String, dynamic>?,
-      userType: json['userType']?.toString(),
+      status: json['success'] == 'true' ? 'success' : 'error',
+      message: json['msg']?.toString() ?? '',
+      data: userData,
+      userType: json['user_type']?.toString(),
       token: json['token']?.toString(),
       myFollowers: myFollowers,
       currency: json['currency']?.toString(),
